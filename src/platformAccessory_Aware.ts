2cae7e97b6e15cb2a89d930526db087177b47b3d
Fraser Sim <fjs21@hotmail.com>
2022-05-03 10:03:03 -0400

    #11

--- src/platformAccessory_Aware.ts
+++ src/platformAccessory_Aware.ts
@@ -273,10 +273,10 @@ export class BlueAirAwareAccessory {
       }
 
       // Characteristic triggers warning if value over 1000      
-      if(this.accessory.context.measurements.pm < 1000){ 
-        this.AirQualitySensor.updateCharacteristic(this.platform.Characteristic.PM2_5Density, this.accessory.context.measurements.pm);
-      } else {
+      if(this.accessory.context.measurements.pm > 1000){ 
         this.AirQualitySensor.updateCharacteristic(this.platform.Characteristic.PM2_5Density, 1000);
+      } else {
+        this.AirQualitySensor.updateCharacteristic(this.platform.Characteristic.PM2_5Density, this.accessory.context.measurements.pm);
       }
 
       // Characteristic triggers warning if value over 1000
