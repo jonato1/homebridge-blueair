c4173e2addd4af7ec500c248029e93e6cd07a148
Fraser Sim <fjs21@hotmail.com>
2022-05-13 15:49:03 -0400

    Update platformAccessory.ts

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -127,7 +127,7 @@ export class BlueAirPlatformAccessory {
         humidity: this.accessory.context.measurements.hum,
         ppm: this.accessory.context.measurements.allpollu,
       });
-    }, 5000);
+    }, 60000);
 
   }
 
